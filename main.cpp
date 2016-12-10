3446d0be9b09d7fdc60c84f52bc4d4631f5446a9
radu <radu.milic@aeche.ro>
2016-12-10 20:37:54 +0200

    hello world

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.cpp
+ * Author: radu
+ *
+ * Created on December 10, 2016, 8:19 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+    
+    cout << "hello world";
+    
+    return 0;
+}
+
